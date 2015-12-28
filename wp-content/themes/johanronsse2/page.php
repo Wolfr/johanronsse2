9bafc885e870a7d3896f9f381efd7a177dd7cc85
Johan Ronsse <johan.ronsse@gmail.com>
2015-12-28 16:30:29 +0100

    Improve theme (iPad testing)

--- wp-content/themes/johanronsse2/page.php
+++ wp-content/themes/johanronsse2/page.php
@@ -5,25 +5,22 @@
         <div class="content-inner">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <?php if (is_page(8) || is_page(669)) { ?>
-                            <?php // Don't show title on projects page ?>
-                        <?php } else { ?>
-                            <header class="header">
-                                <h1 class="entry-title"><?php the_title(); ?></h1> 
-                                <p><?php the_time( get_option( 'date_format' ) ); ?></p>
-                            </header>
-                        <?php } ?>
-                        <section class="entry-content">
-                            <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-                            <?php the_content(); ?>
-
-                            <div class="entry-links"><?php wp_link_pages(); ?></div>
-                            <?php edit_post_link(); ?>
-                        </section>
-                    </article>
-
-                </div>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <?php if (is_page(8) || is_page(669)) { ?>
+                        <?php // Don't show title on projects page ?>
+                    <?php } else { ?>
+                        <header class="header">
+                            <h1 class="entry-title"><?php the_title(); ?></h1> 
+                            <p><?php the_time( get_option( 'date_format' ) ); ?></p>
+                        </header>
+                    <?php } ?>
+                    <section class="entry-content">
+                        <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+                        <?php the_content(); ?>
+                        <div class="entry-links"><?php wp_link_pages(); ?></div>
+                        <?php edit_post_link(); ?>
+                    </section>
+                </article>
 
                 <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
             <?php endwhile; endif; ?>

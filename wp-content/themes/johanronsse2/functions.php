29296f64e377632bf364ab9cb2daf91ac0b0705d
Johan Ronsse <johan.ronsse@gmail.com>
2016-01-03 21:24:15 +0100

    Commit recent changes

--- wp-content/themes/johanronsse2/functions.php
+++ wp-content/themes/johanronsse2/functions.php
@@ -1,67 +1,97 @@
 <?php
+
 add_action( 'after_setup_theme', 'johanronsse_setup' );
-function johanronsse_setup()
-{
-load_theme_textdomain( 'johanronsse', get_template_directory() . '/languages' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-global $content_width;
-if ( ! isset( $content_width ) ) $content_width = 640;
-register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'johanronsse' ) )
-);
+
+function johanronsse_setup() {
+    load_theme_textdomain( 'johanronsse', get_template_directory() . '/languages' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'post-thumbnails' );
+
+    global $content_width;
+    if ( ! isset( $content_width ) ) $content_width = 840;
+    
+    register_nav_menus(
+        array( 'main-menu' => __( 'Main Menu', 'johanronsse' ) )
+    );
 }
+
 add_action( 'wp_enqueue_scripts', 'johanronsse_load_scripts' );
-function johanronsse_load_scripts()
-{
-wp_enqueue_script( 'jquery' );
+
+function johanronsse_load_scripts() {
+    wp_enqueue_script( 'jquery' );
 }
+
 add_action( 'comment_form_before', 'johanronsse_enqueue_comment_reply_script' );
-function johanronsse_enqueue_comment_reply_script()
-{
-if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
+
+function johanronsse_enqueue_comment_reply_script() {
+    if (
+        get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
 }
+
 add_filter( 'the_title', 'johanronsse_title' );
+
 function johanronsse_title( $title ) {
-if ( $title == '' ) {
-return '&rarr;';
-} else {
-return $title;
+    if ( $title == '' ) {
+        return '&rarr;';
+    } else {
+        return $title;
+    }
 }
-}
+
 add_filter( 'wp_title', 'johanronsse_filter_wp_title' );
+
 function johanronsse_filter_wp_title( $title )
 {
-return $title . esc_attr( get_bloginfo( 'name' ) );
+    return $title . esc_attr( get_bloginfo( 'name' ) );
 }
+
 add_action( 'widgets_init', 'johanronsse_widgets_init' );
-function johanronsse_widgets_init()
-{
-register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'johanronsse' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
+
+function johanronsse_widgets_init() {
+    register_sidebar( array (
+    'name' => __( 'Sidebar Widget Area', 'johanronsse' ),
+    'id' => 'primary-widget-area',
+    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+    'after_widget' => "</li>",
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+    ) );
 }
-function johanronsse_custom_pings( $comment )
-{
-$GLOBALS['comment'] = $comment;
+
+function johanronsse_custom_pings( $comment ) {
+    $GLOBALS['comment'] = $comment;
 ?>
 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php 
-}
+<?php }
 add_filter( 'get_comments_number', 'johanronsse_comments_number' );
 function johanronsse_comments_number( $count )
 {
 if ( !is_admin() ) {
 global $id;
 $comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-return count( $comments_by_type['comment'] );
+    return count( $comments_by_type['comment'] );
 } else {
-return $count;
+    return $count;
 }
+}
+
+/**
+ * Remove Emoji Cruft
+ */
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_action( 'wp_head', 'wp_generator');
+
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
 }

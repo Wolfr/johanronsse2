b18fb9c08e39ccefed6d5d262d231d23473e4a69
Wolfr <johan.ronsse@gmail.com>
2018-11-03 19:58:06 +0100

    Improve a11y
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- wp-content/themes/johanronsse2/category.php
+++ wp-content/themes/johanronsse2/category.php
@@ -1,16 +1,20 @@
 <?php get_header(); ?>
-<main id="content" role="main">
+
+<main class="section">
+
     <header class="header">
         <h1 class="entry-title"><?php _e( 'Category Archives: ', 'johanronsse' ); ?><?php single_cat_title(); ?></h1>
         <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
     </header>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part( 'entry' ); ?>
-    <?php endwhile; endif; ?>
+    <ul class="article-list">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <li class="article-list-item"><?php get_template_part( 'entry' ); ?></li>
+        <?php endwhile; endif; ?>
+    </ul>
 
     <?php get_template_part( 'nav', 'below' ); ?>
-</section>
+</main>
 
 <!-- <?php get_sidebar(); ?> -->
 <?php get_footer(); ?>

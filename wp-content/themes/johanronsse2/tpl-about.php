d9087389cc4210b8f0564794eeaa44e36766169d
Wolfr <johan.ronsse@gmail.com>
2019-06-16 15:52:53 +0200

    Change to static content
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- wp-content/themes/johanronsse2/tpl-about.php
+++ wp-content/themes/johanronsse2/tpl-about.php
@@ -9,27 +9,15 @@
 <main class="section">
     <div class="inner">
         <div class="content-inner">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <?php if (is_page(8) || is_page(669)) { ?>
-                            <?php // Don't show title on projects page ?>
-                        <?php } else { ?>
-                            <header class="header">
-                                <h1 class="entry-title"><?php the_title(); ?></h1> 
-                            </header>
-                        <?php } ?>
-                        <section class="entry-content">
-                            <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-                            <?php the_content(); ?>
-                            <?php edit_post_link(); ?>
-                        </section>
-                    </article>
+            <img class="avatar" src="http://johanronsse.be/wp-content/themes/johanronsse2/images/avatar.png" width="128" alt="Johan Ronsse" />
 
-                </div>
+            <p>I am a user interface designer living in Belgium. I work for <a href="https://mono.company/">Mono</a>, which is a company I founded with my business partner <a href="http://xavierbertels.com">Xavier</a>.</p>
 
-                <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-            <?php endwhile; endif; ?>
+            <p>Besides blogging on this this website, sometimes I <a href="http://twitter.com/wolfr_2">tweet</a>.</p>
+
+            <p>You can e-mail me: <a href="mailto:website@johanronsse.be">website@johanronsse.be</a>.</p>
+
         </div>
     </div>
 </main>

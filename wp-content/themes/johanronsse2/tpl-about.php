eeec32ea19dfcb1b9851064d42cd665a6617a6e1
Johan Ronsse <johan.ronsse@gmail.com>
2019-10-30 21:52:32 +0100

    Make content CMS manageable again

--- wp-content/themes/johanronsse2/tpl-about.php
+++ wp-content/themes/johanronsse2/tpl-about.php
@@ -10,10 +10,10 @@
     <div class="inner">
         <div class="content-inner">
 
-            <img class="avatar" src="<?php bloginfo('template_directory') ?>/images/avatar.png" width="128" alt="Johan Ronsse" />
-            <p>I am a user interface designer living in Belgium. I work for <a href="https://mono.company/">Mono</a>, which is a company I founded with my business partner <a href="http://xavierbertels.com">Xavier</a>.</p>
-            <p>Besides blogging on this this website, sometimes I <a href="http://twitter.com/wolfr_2">tweet</a>.</p>
-            <p>You can e-mail me: <a href="mailto:website@johanronsse.be">website@johanronsse.be</a>.</p>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            <?php endwhile; endif; ?>
 
         </div>
     </div>

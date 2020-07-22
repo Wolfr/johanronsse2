1593b76d9531b0f61c47369a1229009127eee523
Johan Ronsse <johan.ronsse@gmail.com>
2020-07-22 11:46:51 +0200

    Add number of comments

--- wp-content/themes/johanronsse2/entry.php
+++ wp-content/themes/johanronsse2/entry.php
@@ -18,9 +18,28 @@
                     echo '</a></h2>';
                 }
             ?>
-        <p><time><?php the_time( get_option( 'date_format' ) ); ?></time> - Posted in <?php the_category(' '); ?></p>
+        <p>
+            <time><?php the_time( get_option( 'date_format' ) ); ?></time> - 
+            Posted in <?php the_category(' '); ?>
+
+            <?php
+                $comments_number = get_comments_number();
+                if ( 1 === $comments_number ) {
+                    printf('- 1 comment');
+                } else if ( 0 === $comments_number ) {
+                    // Do nothing, don't put focus on comments if there are none
+                } else {
+                    echo '- ' . $comments_number . ' comments';
+                }
+            ?>
+            
+        </p>
     </header>
     <section class="entry-content">
         <?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
     </section>
 </article>
+
+
+
+

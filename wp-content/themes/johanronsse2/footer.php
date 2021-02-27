3b8693a446060cd0e349801ae7f1ebb04fb72c55
Wolfr <johan.ronsse@gmail.com>
2021-02-27 21:20:44 +0100

    remove portfolio link
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- wp-content/themes/johanronsse2/footer.php
+++ wp-content/themes/johanronsse2/footer.php
@@ -2,7 +2,6 @@
         <div class="inner">
             <ul>
                 <li>©<?php echo date('Y'); ?> Johan Ronsse</li>
-                <li><a href="http://wolfr_.dribbble.com/">Portfolio</a></li>
                 <li><a href="https://www.instagram.com/wolfr_/">Instagram</a></li>
                 <li><a href="https://twitter.com/wolfr_2">Twitter</a></li>
             </ul>
